4ef20824fc07d77d1395fc8cf60f7e4d4b816dcf
PGH <qkrrudgh30@naver.com>
2025-04-23 13:05:31 +0900

    4-3

--- Source/ShooterX/Character/SXPlayerPawn.cpp
+++ Source/ShooterX/Character/SXPlayerPawn.cpp
@@ -24,11 +24,12 @@ ASXPlayerPawn::ASXPlayerPawn()
     FVector PivotPosition(0.f, 0.f, -CharacterHalfHeight);
     FRotator PivotRotation(0.f, -90.f, 0.f);
     SkeletalMeshComponent->SetRelativeLocationAndRotation(PivotPosition, PivotRotation);
-    //static ConstructorHelpers::FObjectFinder<USkeletalMesh> SkeletalMeshAsset(TEXT("오브젝트 패스"));
-    //if (true == SkeletalMeshAsset.Succeeded())
-    //{
-    //    SkeletalMeshComponent->SetSkeletalMesh(SkeletalMeshAsset.Object);
-    //}
+    static ConstructorHelpers::FClassFinder<UAnimInstance> AnimInstanceClassInfo(TEXT("/Script/Engine.AnimBlueprint'/Game/ShooterX/Animation/AnimationBlueprint/ABP_PlayerPawn.ABP_PlayerPawn_C'"));
+    if (AnimInstanceClassInfo.Succeeded() == true)
+    {
+        SkeletalMeshComponent->SetAnimationMode(EAnimationMode::AnimationBlueprint);
+        SkeletalMeshComponent->SetAnimInstanceClass(AnimInstanceClassInfo.Class);
+    }
 
     SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
     SpringArmComponent->SetupAttachment(RootComponent);
@@ -48,6 +49,11 @@ void ASXPlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 
     PlayerInputComponent->BindAxis(TEXT("UpDown"), this, &ThisClass::UpDown);
     PlayerInputComponent->BindAxis(TEXT("LeftRight"), this, &ThisClass::LeftRight);
+}
+
+void ASXPlayerPawn::BeginPlay()
+{
+    Super::BeginPlay();
 }
 
 void ASXPlayerPawn::UpDown(float InAxisValue)
