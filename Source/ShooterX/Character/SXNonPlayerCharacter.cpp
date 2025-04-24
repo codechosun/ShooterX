91d24d07cb016bf5dc86415b519407a65c2bc764
PGH <qkrrudgh30@naver.com>
2025-04-24 22:24:56 +0900

    8-7

--- Source/ShooterX/Character/SXNonPlayerCharacter.cpp
+++ Source/ShooterX/Character/SXNonPlayerCharacter.cpp
@@ -5,8 +5,10 @@
 
 #include "Controller/SXAIController.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Animation/SXAnimInstance.h"
 
 ASXNonPlayerCharacter::ASXNonPlayerCharacter()
+	: bIsNowAttacking(false)
 {
 	PrimaryActorTick.bCanEverTick = true;
 
@@ -27,5 +29,37 @@ void ASXNonPlayerCharacter::BeginPlay()
 		GetCharacterMovement()->RotationRate = FRotator(0.f, 480.f, 0.f);
 
 		GetCharacterMovement()->MaxWalkSpeed = 300.f;
+	}
+}
+
+void ASXNonPlayerCharacter::BeginAttack()
+{
+	USXAnimInstance* AnimInstance = Cast<USXAnimInstance>(GetMesh()->GetAnimInstance());
+	checkf(IsValid(AnimInstance) == true, TEXT("Invalid AnimInstance"));
+
+	GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_None);
+	if (IsValid(AnimInstance) == true && IsValid(AttackMeleeMontage) == true && AnimInstance->Montage_IsPlaying(AttackMeleeMontage) == false)
+	{
+		AnimInstance->Montage_Play(AttackMeleeMontage);
+
+		bIsNowAttacking = true;
+
+		if (OnAttackMontageEndedDelegate.IsBound() == false)
+		{
+			OnAttackMontageEndedDelegate.BindUObject(this, &ThisClass::EndAttack);
+			AnimInstance->Montage_SetEndDelegate(OnAttackMontageEndedDelegate, AttackMeleeMontage);
+		}
+	}
+}
+
+void ASXNonPlayerCharacter::EndAttack(UAnimMontage* InMontage, bool bInterruped)
+{
+	GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_Walking);
+
+	bIsNowAttacking = false;
+
+	if (OnAttackMontageEndedDelegate.IsBound() == true)
+	{
+		OnAttackMontageEndedDelegate.Unbind();
 	}
 }
--- Source/ShooterX/Character/SXNonPlayerCharacter.h
+++ Source/ShooterX/Character/SXNonPlayerCharacter.h
@@ -6,6 +6,8 @@
 #include "Character/SXCharacterBase.h"
 #include "SXNonPlayerCharacter.generated.h"
 
+DECLARE_DELEGATE_TwoParams(FOnAttackMontageEnded, UAnimMontage*, bool /*bInterrupted*/)
+
 /**
  *
  */
@@ -14,9 +16,23 @@ class SHOOTERX_API ASXNonPlayerCharacter : public ASXCharacterBase
 {
 	GENERATED_BODY()
 
+	friend class UBTTask_Attack;
+
 public:
 	ASXNonPlayerCharacter();
 
 	virtual void BeginPlay() override;
+
+protected:
+	virtual void BeginAttack();
+
+	virtual void EndAttack(UAnimMontage* InMontage, bool bInterruped);
+
+public:
+	bool bIsNowAttacking;
+
+protected:
+	FOnAttackMontageEnded OnAttackMontageEndedDelegate;
+	// 애니메이션 몽타주 재생이 끝났을 때 호출할 델리게이트.
 
 };
