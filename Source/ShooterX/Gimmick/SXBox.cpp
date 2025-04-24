4a23df676d28d9f68e9ed90445d4518127af06dc
PGH <qkrrudgh30@naver.com>
2025-04-24 16:22:52 +0900

    7-4

---
+++ Source/ShooterX/Gimmick/SXBox.cpp
@@ -0,0 +1,41 @@
+// SXBox.cpp
+
+
+#include "Gimmick/SXBox.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+
+ASXBox::ASXBox()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+    SetRootComponent(BoxComponent);
+    BoxComponent->SetBoxExtent(FVector(40.0f, 42.0f, 30.0f));
+    BoxComponent->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnOverlapBegin);
+    //BoxComponent->SetCollisionProfileName(FName("Trigger"));
+    BoxComponent->SetCollisionProfileName(FName(TEXT("SXBox")));
+
+    StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
+    StaticMeshComponent->SetupAttachment(GetRootComponent());
+    StaticMeshComponent->SetRelativeLocation(FVector(0.0f, -3.5f, -30.0f));
+    StaticMeshComponent->SetCollisionProfileName(TEXT("NoCollision"));
+
+    ParticleSystemComponent = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("ParticleSystemComponent"));
+    ParticleSystemComponent->SetupAttachment(GetRootComponent());
+    ParticleSystemComponent->SetAutoActivate(false);
+}
+
+void ASXBox::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepHitResult)
+{
+    ParticleSystemComponent->Activate(true);
+    StaticMeshComponent->SetHiddenInGame(true);
+    SetActorEnableCollision(false);
+    ParticleSystemComponent->OnSystemFinished.AddDynamic(this, &ThisClass::OnEffectFinish);
+}
+
+void ASXBox::OnEffectFinish(UParticleSystemComponent* ParticleSystem)
+{
+    Destroy();
+}
