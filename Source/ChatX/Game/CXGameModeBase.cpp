be9554540a18e2fd89af7f8eb8023a05102cea0a
PGH <qkrrudgh30@naver.com>
2025-04-26 01:01:07 +0900

    11-1

---
+++ Source/ChatX/Game/CXGameModeBase.cpp
@@ -0,0 +1,5 @@
+// CXGameModeBase.cpp
+
+
+#include "Game/CXGameModeBase.h"
+
---
+++ Source/ChatX/Player/CXPlayerController.h
@@ -0,0 +1,34 @@
+// CXPlayerController.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CXPlayerController.generated.h"
+
+class UCXChatInput;
+
+/**
+ *
+ */
+UCLASS()
+class CHATX_API ACXPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	virtual void BeginPlay() override;
+
+	void SetChatMessageString(const FString& InChatMessageString);
+
+	void PrintChatMessageString(const FString& InChatMessageString);
+
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UCXChatInput> ChatInputWidgetClass;
+
+	UPROPERTY()
+	TObjectPtr<UCXChatInput> ChatInputWidgetInstance;
+
+	FString ChatMessageString;
+};
